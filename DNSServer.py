d134b9a3ef323da674fc9bcd536905f985c07dec
kashj27 <kj2714@nyu.edu>
2026-03-18 19:13:59 -0400

    Complete Assignment 4,DNS part 2

--- DNSServer.py
+++ DNSServer.py
@@ -96,6 +96,10 @@ dns_records = {
     'nyu.edu.': {
         dns.rdatatype.A: '192.168.1.106',
         dns.rdatatype.TXT: encrypted_value,
+        dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0370:7334',
+        dns.rdatatype.MX: [(10, 'mail.nyu.edu.')],  # List of (preference, mail server) tuples
+        dns.rdatatype.CNAME: 'www.nyu.edu.',
+        dns.rdatatype.NS: 'ns.nyu.edu.',
     },
 
     # Add more records as needed (see assignment instructions!
